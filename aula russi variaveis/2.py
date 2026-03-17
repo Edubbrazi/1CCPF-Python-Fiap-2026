991db4e5aa26057eb5bbec12506c4b8e15a6f9cb
Edubbrazi <edubbrazi@gmail.com>
2026-03-17 11:41:09 -0300

    aula 02 russi

---
+++ aula russi variaveis/2.py
@@ -0,0 +1,4 @@
+dia = int(input("Digite o dia de seu aniversario"))
+mes = int(input("Digite o mes de seu aniversario"))
+ano = int(input("Digite o ano de seu nascimento"))
+print(f"Sua data de nascimento é {dia}/{mes}/{ano}")
